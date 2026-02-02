6e4293e07df9b28ed1adaef6e2cc7562c29a1260
Bb-jh <august0511@naver.com>
2026-02-03 04:33:10 +0900

    docs: 학력, 자격증, 경력에 스웨거 태그, 설명 추가

--- src/main/java/CamNecT/CamNecT_Server/domain/certificate/controller/CertificateController.java
+++ src/main/java/CamNecT/CamNecT_Server/domain/certificate/controller/CertificateController.java
@@ -5,6 +5,8 @@ import CamNecT.CamNecT_Server.domain.certificate.dto.response.CertificateRespons
 import CamNecT.CamNecT_Server.domain.certificate.service.CertificateService;
 import CamNecT.CamNecT_Server.domain.users.model.CustomUserDetails;
 import CamNecT.CamNecT_Server.global.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Certificate", description = "자격증 정보 관련 API")
 @RestController
 @RequestMapping("/api/user/me/certificates")
 @RequiredArgsConstructor
@@ -19,7 +22,10 @@ public class CertificateController {
 
     private final CertificateService certificateService;
 
-    // 학력 조회
+    @Operation(
+            summary = "내 자격증 목록 조회",
+            description = "현재 로그인한 사용자가 등록한 모든 자격증 리스트를 조회합니다."
+    )
     @GetMapping
     public ApiResponse<List<CertificateResponse>> getMyCertificates(
             @AuthenticationPrincipal CustomUserDetails userDetails
@@ -28,7 +34,10 @@ public class CertificateController {
         return ApiResponse.success(response);
     }
 
-    // 학력 추가
+    @Operation(
+            summary = "자격증 정보 추가",
+            description = "새로운 자격증 취득 정보를 추가합니다."
+    )
     @PostMapping
     public ApiResponse<Void> addCertificate(
             @AuthenticationPrincipal CustomUserDetails userDetails,
@@ -38,7 +47,10 @@ public class CertificateController {
         return ApiResponse.success(null);
     }
 
-    // 학력 수정
+    @Operation(
+            summary = "자격증 정보 수정",
+            description = "기존에 등록된 특정 자격증 정보를 수정합니다."
+    )
     @PatchMapping("/{certificateId}")
     public ApiResponse<Void> updateCertificate(
             @AuthenticationPrincipal CustomUserDetails userDetails,
@@ -49,7 +61,10 @@ public class CertificateController {
         return ApiResponse.success(null);
     }
 
-    // 학력 삭제
+    @Operation(
+            summary = "자격증 정보 삭제",
+            description = "등록된 자격증 정보 중 하나를 삭제합니다."
+    )
     @DeleteMapping("/{certificateId}")
     public ApiResponse<Void> deleteCertificate(
             @AuthenticationPrincipal CustomUserDetails userDetails,
