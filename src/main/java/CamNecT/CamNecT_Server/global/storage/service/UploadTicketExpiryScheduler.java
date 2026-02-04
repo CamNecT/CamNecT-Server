29a4f10e64f9d7877c9c616041fde53b16f86b68
seungmo <boorue03@gmail.com>
2026-02-04 19:28:58 +0900

    feat : UploadTicket 상태 개선

---
+++ src/main/java/CamNecT/CamNecT_Server/global/storage/service/UploadTicketExpiryScheduler.java
@@ -0,0 +1,22 @@
+package CamNecT.CamNecT_Server.global.storage.service;
+
+import CamNecT.CamNecT_Server.global.storage.repository.UploadTicketRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class UploadTicketExpiryScheduler {
+
+    private final UploadTicketRepository ticketRepo;
+
+    @Scheduled(fixedDelayString = "${app.upload-ticket.expire-job-delay-ms:300000}")
+    @Transactional
+    public void expirePendingTickets() {
+        ticketRepo.bulkExpirePending(LocalDateTime.now());
+    }
+}
