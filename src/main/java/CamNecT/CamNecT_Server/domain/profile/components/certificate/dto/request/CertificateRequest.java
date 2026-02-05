e9191b436f7d9fcca44a17d5341fadd929b3f36d
Bb-jh <august0511@naver.com>
2026-02-06 04:08:42 +0900

    fix: Dto에서 userId 제거

--- src/main/java/CamNecT/CamNecT_Server/domain/profile/components/certificate/dto/request/CertificateRequest.java
+++ src/main/java/CamNecT/CamNecT_Server/domain/profile/components/certificate/dto/request/CertificateRequest.java
@@ -3,12 +3,11 @@ package CamNecT.CamNecT_Server.domain.profile.components.certificate.dto.request
 import java.time.LocalDate;
 
 public record CertificateRequest(
-        Long userId,
         String certificateName,
         LocalDate acquiredDate,
         String credentialUrl
-//        String issuerName,
-//        LocalDate expireDate,
-//        String description
+/*        String issuerName,
+        LocalDate expireDate,
+        String description*/
 ) {
 }
