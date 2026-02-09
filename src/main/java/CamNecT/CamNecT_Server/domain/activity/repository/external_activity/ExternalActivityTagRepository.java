706860d8a92d58312b75f4fc990a92f7daa6e680
seungmo <boorue03@gmail.com>
2026-02-09 20:27:36 +0900

    fix : 북마크,작성글 도메인(API) 제작에 따른 수정

--- src/main/java/CamNecT/CamNecT_Server/domain/activity/repository/external_activity/ExternalActivityTagRepository.java
+++ src/main/java/CamNecT/CamNecT_Server/domain/activity/repository/external_activity/ExternalActivityTagRepository.java
@@ -12,4 +12,5 @@ public interface ExternalActivityTagRepository extends JpaRepository<ExternalAct
     void deleteByActivity_ActivityId(Long activityId);
 
     List<ExternalActivityTag> findAllByActivity_ActivityId(Long activityId);
+    List<ExternalActivityTag> findAllByActivity_ActivityIdIn(List<Long> activityIds);
 }
