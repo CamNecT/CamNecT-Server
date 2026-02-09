6e90516659b7c9dc9df23b13b284ad7cef200907
Bb-jh <august0511@naver.com>
2026-02-09 17:46:21 +0900

    feat: status가 OPEN인 채팅방만 조회하도록 수정 (전체, 타입 조회)

--- src/main/java/CamNecT/CamNecT_Server/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/CamNecT/CamNecT_Server/domain/chat/repository/ChatRoomRepository.java
@@ -10,30 +10,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-/*    @Query("SELECT cr FROM ChatRoom cr " +
-            "WHERE cr.requester.userId = :userId OR cr.receiver.userId = :userId " +
-            "ORDER BY cr.lastMessageAt DESC")
-    List<ChatRoom> findMyChatRooms(@Param("userId") Long userId);
 
-    @Query("SELECT r FROM ChatRoom r " +
-            "JOIN FETCH r.request req " +
-            "JOIN FETCH req.requestInterests " +
-            "WHERE r.requester.userId = :userId OR r.receiver.userId = :userId")
-    List<ChatRoom> findAllByUserIdWithRequest(@Param("userId") Long userId);*/
-
-/*    @Query("SELECT DISTINCT r FROM ChatRoom r " +
-            "JOIN FETCH r.request req " +
-            "LEFT JOIN FETCH req.requestInterests " +
-            "JOIN FETCH r.requester " +
-            "JOIN FETCH r.receiver " +
-            "WHERE r.requester.userId = :userId OR r.receiver.userId = :userId")
-    List<ChatRoom> findAllByUserIdWithBasicInfo(@Param("userId") Long userId);*/
-
+    // 전체 조회
     @Query("SELECT r FROM ChatRoom r " +
             "JOIN FETCH r.request " +
             "JOIN FETCH r.requester " +
             "JOIN FETCH r.receiver " +
             "WHERE r.requester.userId = :userId OR r.receiver.userId = :userId " +
+            "AND r.status = 'OPEN' " +
             "ORDER BY r.lastMessageAt DESC")
     List<ChatRoom> findAllByUserIdWithBasicInfo(@Param("userId") Long userId);
 
@@ -44,10 +28,12 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
             "JOIN FETCH r.receiver " +
             "WHERE (r.requester.userId = :userId OR r.receiver.userId = :userId) " +
             "AND req.type = :type " +
+            "AND r.status = 'OPEN' " +
             "ORDER BY r.lastMessageAt DESC")
     List<ChatRoom> findAllByUserIdAndType(@Param("userId") Long userId,
                                           @Param("type") ChatRequest.RequestType type);
 
+    // 상세 조회
     @Query("SELECT r FROM ChatRoom r " +
             "JOIN FETCH r.requester " +
             "JOIN FETCH r.receiver " +
