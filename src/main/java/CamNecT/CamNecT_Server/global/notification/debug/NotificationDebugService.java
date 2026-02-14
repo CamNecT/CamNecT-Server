72e96580fbdb5b5739638f58ba7f0a2eafea41b6
seungmo <boorue03@gmail.com>
2026-02-14 22:54:04 +0900

    fix : 서버 다운 핫픽스

--- src/main/java/CamNecT/CamNecT_Server/global/notification/debug/NotificationDebugService.java
+++ src/main/java/CamNecT/CamNecT_Server/global/notification/debug/NotificationDebugService.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
 
-@Profile({"local", "dev"})
+//@Profile({"local", "dev"})
 @Service
 @RequiredArgsConstructor
 public class NotificationDebugService {
