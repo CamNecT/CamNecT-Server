b8f941a4ac2da804c166fba2b651b84629df8be0
seungmo <boorue03@gmail.com>
2026-02-16 00:23:44 +0900

    fix : 태그 중복 에러 수정(대외활동)

--- src/main/java/CamNecT/server/domain/activity/repository/external_activity/ExternalActivityTagRepository.java
+++ src/main/java/CamNecT/server/domain/activity/repository/external_activity/ExternalActivityTagRepository.java
@@ -2,6 +2,9 @@ package CamNecT.server.domain.activity.repository.external_activity;
 
 import CamNecT.server.domain.activity.model.external_activity.ExternalActivityTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +12,9 @@ import java.util.List;
 @Repository
 public interface ExternalActivityTagRepository extends JpaRepository<ExternalActivityTag, Long> {
 
-    void deleteByActivity_ActivityId(Long activityId);
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("delete from ExternalActivityTag e where e.activity.activityId = :activityId")
+    void deleteAllByActivityId(@Param("activityId") Long activityId);
 
     List<ExternalActivityTag> findAllByActivity_ActivityId(Long activityId);
     List<ExternalActivityTag> findAllByActivity_ActivityIdIn(List<Long> activityIds);
