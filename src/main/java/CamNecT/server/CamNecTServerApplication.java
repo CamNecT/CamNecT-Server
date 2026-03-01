1a94f86427bafc1895e185b4e71c452cbef7ef0a
seungmo <boorue03@gmail.com>
2026-03-02 00:58:27 +0900

    fix : 코드정리

--- src/main/java/CamNecT/server/CamNecTServerApplication.java
+++ src/main/java/CamNecT/server/CamNecTServerApplication.java
@@ -1,8 +1,6 @@
 package CamNecT.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
