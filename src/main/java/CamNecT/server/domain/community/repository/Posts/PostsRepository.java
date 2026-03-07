50467ef923ba11277ca435ee86beebb08b458942
seungmo <boorue03@gmail.com>
2026-03-07 17:51:01 +0900

    chore : 코드정리

--- src/main/java/CamNecT/server/domain/community/repository/Posts/PostsRepository.java
+++ src/main/java/CamNecT/server/domain/community/repository/Posts/PostsRepository.java
@@ -6,11 +6,8 @@ import CamNecT.server.domain.community.model.enums.PostStatus;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.time.LocalDateTime;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
