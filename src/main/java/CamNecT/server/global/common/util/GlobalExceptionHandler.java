0efa67899fbf2d2093a7f47b762f467902e9d59d
YoonBoorue <boorue03@gmail.com>
2026-07-11 17:33:38 +0900

    fix : errorcode 정리

--- src/main/java/CamNecT/server/global/common/util/GlobalExceptionHandler.java
+++ src/main/java/CamNecT/server/global/common/util/GlobalExceptionHandler.java
@@ -1,13 +1,27 @@
 package CamNecT.server.global.common.util;
 
 import CamNecT.server.global.common.exception.CustomException;
-import CamNecT.server.global.common.response.errorcode.BaseErrorCode;
 import CamNecT.server.global.common.response.ErrorResponse;
+import CamNecT.server.global.common.response.errorcode.BaseErrorCode;
+import CamNecT.server.global.common.response.errorcode.ErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,29 +29,75 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustom(CustomException e, HttpServletRequest req) {
+        BaseErrorCode errorCode = e.getErrorCode();
 
-        BaseErrorCode ec = e.getErrorCode();
-
-        // 토큰 원문은 절대 찍지 마세요. (있/없만)
-        String auth = req.getHeader("Authorization");
-        boolean hasAuth = (auth != null && !auth.isBlank());
-
-        Object userIdAttr = req.getAttribute("userId"); // AuthInterceptor에서 setAttribute 한 값
-
-        log.warn("[CustomException] {} {} | status={} code={} | userIdAttr={} | hasAuth={} | ua={}",
+        log.warn("[CustomException] {} {} | status={} code={}",
                 req.getMethod(),
                 req.getRequestURI(),
-                ec.getHttpStatus().value(),
-                ec.getCode(),
-                userIdAttr,
-                hasAuth,
-                req.getHeader("User-Agent")
-        );
+                errorCode.getHttpStatus().value(),
+                errorCode.getCode());
 
-        // 원인 파악 필요하면(개발/스테이징만) cause도 같이:
-        // log.warn("cause=", e);
+        return response(errorCode);
+    }
 
-        return ResponseEntity.status(ec.getHttpStatus())
-                .body(new ErrorResponse(ec.getHttpStatus().value(), ec.getCode(), ec.getMessage()));
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            BindException.class,
+            ConstraintViolationException.class,
+            MissingServletRequestParameterException.class,
+            MissingPathVariableException.class,
+            MissingRequestHeaderException.class,
+            MethodArgumentTypeMismatchException.class
+    })
+    public ResponseEntity<ErrorResponse> handleBadRequest(Exception e, HttpServletRequest req) {
+        log.warn("[BadRequest] {} {} | type={}",
+                req.getMethod(), req.getRequestURI(), e.getClass().getSimpleName());
+        return response(ErrorCode.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(org.springframework.http.converter.HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleUnreadableBody(Exception e, HttpServletRequest req) {
+        log.warn("[UnreadableBody] {} {}", req.getMethod(), req.getRequestURI());
+        return response(ErrorCode.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleMethodNotAllowed(Exception e) {
+        return response(ErrorCode.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public ResponseEntity<ErrorResponse> handleNotAcceptable(Exception e) {
+        return response(ErrorCode.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleUnsupportedMediaType(Exception e) {
+        return response(ErrorCode.UNSUPPORTED_MEDIA_TYPE);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorResponse> handlePayloadTooLarge(Exception e) {
+        return response(ErrorCode.PAYLOAD_TOO_LARGE);
+    }
+
+    @ExceptionHandler({NoHandlerFoundException.class, NoResourceFoundException.class})
+    public ResponseEntity<ErrorResponse> handleNotFound(Exception e) {
+        return response(ErrorCode.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleUnexpected(Exception e, HttpServletRequest req) {
+        log.error("[UnexpectedException] {} {}", req.getMethod(), req.getRequestURI(), e);
+        return response(ErrorCode.INTERNAL_ERROR);
+    }
+
+    private ResponseEntity<ErrorResponse> response(BaseErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(new ErrorResponse(
+                        errorCode.getHttpStatus().value(),
+                        errorCode.getCode(),
+                        errorCode.getMessage()
+                ));
     }
 }
